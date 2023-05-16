531a04b5d2b9323b5c3e569ab145025f9007d003
zhewenhu <larried1111@gmail.com>
2023-05-15 23:14:35 -0500

    lab7: minor fix

--- lab7/bstmap/BSTMap.java
+++ lab7/bstmap/BSTMap.java
@@ -97,4 +97,6 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
     public Iterator<K> iterator()  {
         throw new UnsupportedOperationException();
     }
+
+    public void printInOrder() {}
 }
